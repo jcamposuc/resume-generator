69fda47d8a3bd2dae493dfc3be4c143a76117145
Junior Campos <jcamposuc>
2024-11-03 11:25:18 -0500

    feat: add form utilities

---
+++ src/utilities.js
@@ -0,0 +1,121 @@
+import { useFormState } from "react-hook-form";
+import ControlledInputText from "./components/Common/ControlledInputText";
+
+const FieldError = ({ fieldName, required, control, }) => {
+    const [objName, propName] = fieldName.split(".");
+    const { errors } = useFormState({ control });
+
+    return (
+        required && errors?.[objName]?.[propName]?.type === "required" && (
+            <small className="mr-auto font-semibold text-red-900">*Required field</small>
+        )
+    )
+}
+
+const InputLabel = ({ fieldName, labelText, required }) => (
+    <label
+        className='mb-2 ml-0 text-sm font-medium text-gray-900 dark:text-white '
+        htmlFor={fieldName}>{required && "*"}{labelText}</label>
+)
+
+export const InputText = ({ type = "text", placeholder, fieldName, labelText, register, control, required = true, minLength = 2, maxLength = 256 }) => {
+    return (
+        <div>
+            <InputLabel
+                fieldName={fieldName}
+                labelText={labelText}
+                required={required} />
+            <input
+                type={type}
+                placeholder={placeholder || labelText}
+                className="block w-full p-2 text-base text-gray-900 border border-gray-300 rounded-sm placeholder:text-xs bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                {...register(fieldName, {
+                    required,
+                    minLength,
+                    maxLength,
+                })} />
+            <FieldError
+                fieldName={fieldName}
+                required={required}
+                control={control} />
+        </div>
+    )
+};
+
+export const InputEmail = ({ fieldName, labelText, register, required = true, minLength = 2, maxLength = 50, control }) => {
+    return (
+        <>
+            <InputLabel
+                fieldName={fieldName}
+                labelText={labelText}
+                required={required} />
+            <input
+                type='email'
+                placeholder="reallygreat@gmail.com"
+                className="block w-full p-2 text-base text-gray-900 border border-gray-300 rounded-sm placeholder:text-xs bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                {...register(fieldName, {
+                    required,
+                    minLength,
+                    maxLength,
+                })} />
+            <FieldError
+                fieldName={fieldName}
+                required={required}
+                control={control} />
+        </>
+    )
+};
+
+export const InputPhone = ({ fieldName, labelText, register, control, required = true, minLength = 10, maxLength = 20 }) => {
+    return (
+        <div>
+            <InputLabel
+                fieldName={fieldName}
+                labelText={labelText}
+                required={required} />
+            <input
+                type='tel'
+                placeholder="Format: (+57 ) 304-368-2547"
+                className="block w-full p-2 text-base text-gray-900 border border-gray-300 rounded-sm placeholder:text-xs bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                {...register(fieldName, {
+                    required,
+                    minLength,
+                    maxLength,
+                })} />
+            <FieldError
+                fieldName={fieldName}
+                required={required}
+                control={control} />
+        </div>
+    )
+};
+
+export const InputTextarea = ({ fieldName, labelText, register, control, required = true, minLength = 2, maxLength = 400 }) => {
+    return (
+        <div>
+            <InputLabel
+                fieldName={fieldName}
+                labelText={labelText}
+                required={required} />
+            <textarea
+                rows="3"
+                className="block w-full p-2 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                {...register(fieldName, {
+                    required,
+                    minLength,
+                    maxLength,
+                })} />
+            <FieldError
+                fieldName={fieldName}
+                required={required}
+                control={control} />
+        </div>
+    )
+};
+
+export const getControlledFields = (fields) => (
+    fields.map((field, index) => (
+        <ControlledInputText key={`cit-${index}`}
+            {...field} />
+    ))
+);
