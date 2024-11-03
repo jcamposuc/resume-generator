eb793830d8738d8e7e52b72e1da7b28eff1f1bc1
Junior Campos <jcamposuc>
2024-11-03 11:28:04 -0500

    feat: add main page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { CardTemplate } from './components/CardTemplate';
+import tp1 from './images/template-1.webp';
+import tp2 from './images/template-2.png';
+import tp3 from './images/template-3.jpg';
+import { Layout } from './components/Layout';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Layout>
+    <ul className="grid grid-cols-1 gap-3 px-3 md:px-0 lg:grid-cols-6">
+      <li className='inline-flex lg:block lg:col-span-3 xl:col-span-2 xl:col-start-2'>
+        <CardTemplate
+          imgSrc={tp1}
+          path="/fill-resume?tp=tp-1" />
+      </li>
+      <li className='inline-flex lg:block lg:col-span-3 xl:col-span-2 lg:col-start-4'>
+        <CardTemplate
+          imgSrc={tp2}
+          path="/fill-resume?tp=tp-2"
+          disabled={true} />
+      </li>
+      <li className='inline-flex lg:block lg:col-span-3 xl:col-start-2 xl:col-span-2'>
+        <CardTemplate
+          imgSrc={tp3}
+          path="/fill-resume?tp=tp-3"
+          disabled={true} />
+      </li>
+    </ul>
+  </Layout>
+);
 
 export default App;
