646b4f9af78059be3516da0bbb8cf8469f737862
Junior Campos <junior.uc.9192@gmail.com>
2024-11-06 22:19:29 -0500

    feat: remove template 3

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import { CardTemplate } from './components/CardTemplate';
 import tp1 from './images/template-1.webp';
 import tp2 from './images/template-2.png';
-import tp3 from './images/template-3.jpg';
 import { Layout } from './components/Layout';
 
 const App = () => (
   <Layout>
-    <ul className="grid grid-cols-1 gap-3 px-3 md:px-0 lg:grid-cols-6">
-      <li className='inline-flex lg:block lg:col-span-3 xl:col-span-2 xl:col-start-2'>
+    <ul className="flex flex-col items-center gap-5 px-3 md:px-0">
+      <li>
         <CardTemplate
           imgSrc={tp1}
           path="/fill-resume?tp=tp-1" />
       </li>
-      <li className='inline-flex lg:block lg:col-span-3 xl:col-span-2 lg:col-start-4'>
+      <li>
         <CardTemplate
           imgSrc={tp2}
           path="/fill-resume?tp=tp-2"
-          disabled={true} />
-      </li>
-      <li className='inline-flex lg:block lg:col-span-3 xl:col-start-2 xl:col-span-2'>
-        <CardTemplate
-          imgSrc={tp3}
-          path="/fill-resume?tp=tp-1"
           disabled={true} />
       </li>
     </ul>
